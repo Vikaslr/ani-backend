9f9dfcefb28e572a9c75c1d794928c84cab7caf2
vikas <vikaslr1718@gmail.com>
2024-05-07 21:40:19 +0530

    created repository

--- src/main/java/com/ani/backend/repositories/PropertyRepository.java
+++ src/main/java/com/ani/backend/repositories/PropertyRepository.java
@@ -1,9 +1,8 @@
 package com.ani.backend.repositories;
 
+import com.ani.backend.dao.Property;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
-
-import com.ani.backend.dao.Property;
 
 @Repository
 public interface PropertyRepository extends JpaRepository<Property, Long> {
